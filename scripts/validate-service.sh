e6a7bf8b30434ec81990b77e2f0afda1145772a2
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-04 20:25:20 +0900

    fix: CD 프로세스 수정

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Get the current date and time
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-# Specify the absolute path for the log file
 LOG_FILE="/home/ubuntu/build/deployment-log.txt"
 
-# Log the timestamp to the log file
 echo "Deployment timestamp: $TIMESTAMP" >> $LOG_FILE
 
-# Exit with success (0) or failure (non-zero) status based on your checks
 exit 0
