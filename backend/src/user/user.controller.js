63e2040b3adc9fd455c5fc21923ad1c6d730daad
unknown <hyunjunhwang1994@gmail.com>
2023-11-07 22:50:15 +0900

    fix: cookie domain 설정

--- backend/src/user/user.controller.js
+++ backend/src/user/user.controller.js
@@ -50,7 +50,7 @@ class UserController {
       res.cookie("authorization", token, {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
-        domain: `.${process.env.FRONTEND_SERVER_IP}`,
+        domain: `${process.env.FRONTEND_SERVER_IP}`,
         path: "/",
         sameSite: "none",
         secure: true
