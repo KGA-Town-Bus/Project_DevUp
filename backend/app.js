11e8fcb80c55cedb42b705400800b8268fdf4eba
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-07 16:27:02 +0900

    feat: 프런트 엔드 및 백엔드 jwt 미들웨어 적용 완료

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express =require("express")
 const app = express()
 const router = require("./src/index")
+
 const {swaggerUi ,backSpecs} = require("./swagger/swagger")
+const {auth} = require("./src/lib/jwtAuthMiddleware");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(auth)
+
 app.use(router)
 
 app.use("/api-docs", swaggerUi.serveFiles(backSpecs), swaggerUi.setup(backSpecs, {explorer: true}))
--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,9 @@ const app = express();
 const router = require('./src/index');
 const {swaggerUi, frontSpecs} = require('./swagger/swagger');
 const nunjucks = require('nunjucks');
+const {auth} = require("./src/lib/jwtAuthMiddleware");
+const cookieParser = require("cookie-parser");
+
 
 app.set('view engine', 'html');
 nunjucks.configure('frontend/views', {express: app});
@@ -10,6 +13,11 @@ nunjucks.configure('frontend/views', {express: app});
 app.use(express.static('frontend/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(auth)
+
+
 app.use(router);
 app.use(
   '/api-docs',
