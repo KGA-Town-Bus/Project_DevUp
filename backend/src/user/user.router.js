e1b8f282e4eda98f488b329a0e245a9c4021e177
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-08 13:56:53 +0900

    feat: userProfile 이미지 업로드 및 비동기 변경 구현 완료

--- backend/src/user/user.router.js
+++ backend/src/user/user.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
+const upload = require("../lib/upload")
 
 const {userController} = require('./user.module');
 const postSignup = userController.postSignup.bind(userController);
 const login = userController.login.bind(userController);
+const postProfile = userController.postProfile.bind(userController)
 
 /**
  * @swagger
@@ -49,7 +51,12 @@ const login = userController.login.bind(userController);
  */
 userRouter.post('/', postSignup);
 
+
+userRouter.post("/profile", upload.single("profile"), postProfile)
+
+
 userRouter.get('/:provider', login);
 userRouter.post('/:provider', login);
+
 
 module.exports = userRouter;
