0d189fba52cae97d52b53444e5bbbb2c644d4acb
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-09 09:16:25 +0900

    chore: 코드 정리

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-require('dotenv').config();
 const db = require('./backend/src/lib/db');
 const frontApp = require('./frontend/app');
 const backApp = require('./backend/app');
 
-const frontPort = process.env.FRONTEND_SERVER_PORT;
-const backPort = process.env.BACKEND_SERVER_PORT;
 
-// frontApp.listen(frontPort, () => {
-  frontApp.listen(3000, () => {
-  // console.log(`Frontend START: ${frontPort}`);
+frontApp.listen(3000, () => {
+  console.log(`Frontend START: 3000`);
 });
 
-// backApp.listen(backPort, async () => {
 backApp.listen(4000, async () => {
   await db.sequelize.sync({force: false});
-  // console.log(`Backend START: ${backPort}`);
+  console.log(`Backend START: 4000`);
 });
