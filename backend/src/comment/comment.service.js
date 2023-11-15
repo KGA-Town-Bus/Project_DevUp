6af3c0739f3e79137457565dbd568c69de47a628
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-15 17:28:56 +0900

    feat: post/view/comment/infinte-scroll 구현 완료

--- backend/src/comment/comment.service.js
+++ backend/src/comment/comment.service.js
@@ -2,29 +2,39 @@ const {CommentListResponseDTO} = require("./dto/comment.list.response.dto");
 const db = require("../lib/db")
 const {Logger} = require("sequelize/lib/utils/logger");
 const {CommentResponseDTO} = require("./dto/comment.response.dto");
+const {Op} = require("sequelize");
 
 class CommentService {
   constructor(Comment) {
     this.commentRepository = Comment
   }
 
-  async commentList(postUid) {
+  async commentList(postUid, page) {
+    try {
+      const pageSize = 5;
+      const offset = (page - 1) * pageSize;
 
-    const commentList = await this.commentRepository.findAll({
-      include: [{
-        model: db.Users,
-        attributes: ["Users_nickname", "Users_profile"]
-      }],
-      where: {
-        Posts_uid: postUid
-      },
-      order: [['Comments_created_at', 'DESC']],
-    })
+      const commentList = await this.commentRepository.findAll({
+        include: [{
+          model: db.Users,
+          attributes: ["Users_nickname", "Users_profile"]
+        }],
+        offset: offset,
+        limit: pageSize,
+        where: {
+          Posts_uid: postUid
+        },
+        order: [['Comments_created_at', 'DESC']],
+      })
 
-    return commentList.map((comment) => {
-      return new CommentListResponseDTO(comment.dataValues)
-    })
+      return commentList.map((comment) => {
+        return new CommentListResponseDTO(comment.dataValues)
+      })
+    } catch (e) {
+      throw e
+    }
   }
+
 
   async createComment(requestDTO) {
     try {
@@ -38,7 +48,7 @@ class CommentService {
 
       const {dataValues} = await this.commentRepository.findOne({
         include: {
-          model : db.Users,
+          model: db.Users,
           attributes: ["Users_nickname", "Users_profile"]
         },
         where: {
