bf8cd441574e77fdec9a1abcb97d132811f73bd0
Eunjae Lee <vanitasium@gmail.com>
2023-11-15 02:17:02 -0500

    feat: chat 구현중

--- frontend/public/js/chat/client.js
+++ frontend/public/js/chat/client.js
@@ -8,51 +8,36 @@ const form = document.querySelector('.chat-form');
 const input = document.querySelector('.chat-input');
 const messages = document.getElementById('messages');
 const chatBody = document.querySelector('.chat-body');
+let currentUser = null;
 
+// Handle user information
+socket.on('userinfo', user => {
+  console.log('userinfo received at the client');
+  currentUser = user; // Store the current user's information
+  socket.emit('register', user.Users_uid);
+});
+
+// Handle incoming chat messages
+socket.on('chat message', (msg, date, senderInfo) => {
+  const item = `<div class="message-area">
+    <div class="message-user">
+      <img src="${senderInfo.profile}" class="msg-pfp"></img>
+      <span class="msg-username">${senderInfo.nickname}</span>
+    </div>
+    <div class="message-date"><span>${date}</span></div>
+    <div class="message"><span>${msg}</span></div>
+  </div>`;
+  messages.innerHTML += item;
+  chatBody.scrollTop = chatBody.scrollHeight;
+});
+
+// Send message
 form.addEventListener('submit', e => {
-  console.log('so this is working?');
   e.preventDefault();
   if (input.value) {
-    console.log(input.value);
-
     socket.emit('chat message', input.value);
-
-    console.log('socket emitted?');
     input.value = '';
   }
 });
 
-socket.on('userinfo', user => {
-  console.log('userinfo received at the client');
-
-  socket.on('chat message', (msg, date) => {
-    const item = `<div class="message-area">
-    <div class="message-user">
-      <img src="${user.Users_profile}" class="msg-pfp"></img>
-      <span class="msg-username">${user.Users_nickname}</span>
-    </div>
-  
-    <div class="message-date"><span>${date}</span></div>
-  
-    <div class="message">
-      <span
-        >${msg}</span
-      >
-    </div>
-  </div>`;
-    messages.innerHTML += item;
-    chatBody.scrollTop = chatBody.scrollHeight;
-  });
-});
-
-// socket.on('roommsg', data => {
-//   console.log(data);
-// });
-
-// socket.on('excluderoom', data => {
-//   console.log(data);
-// });
-
-// socket.on('leaveroom', () => {
-//   console.log('you left the room');
-// });
+// Additional socket event handlers can be added here
