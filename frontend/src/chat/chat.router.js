4cf8cffcf479acaef0462d1c771ac4ed9a67b396
아기참돔 <wjdtmdry88@gmail.com>
2023-11-15 19:09:33 +0900

    fix: 채팅 수정

--- frontend/src/chat/chat.router.js
+++ frontend/src/chat/chat.router.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const chatRouter = express.Router();
 
+require('dotenv').config();
+
+const BACKEND_SERVER_IP = process.env.BACKEND_SERVER_IP;
+const BACKEND_SERVER_PORT = process.env.BACKEND_SERVER_PORT;
+const PROTOCOL = process.env.PROTOCOL;
+
+const backServer = {
+  PROTOCOL,
+  BACKEND_SERVER_IP,
+  BACKEND_SERVER_PORT,
+};
+
 chatRouter.get('/', (req, res) => {
   const user = req.user;
-  res.render('chat/client.html', {user});
+  res.render('chat/client.html', {user, backServer});
 });
 
 module.exports = chatRouter;
