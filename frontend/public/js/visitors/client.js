79640f4386c5c08b786c519b61630c2d3004756f
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-17 09:27:58 +0900

    fix: 유저 접속 리스트 view 페이지에도 적용

--- frontend/public/js/visitors/client.js
+++ frontend/public/js/visitors/client.js
@@ -1,4 +1,4 @@
-if(localStorage.getItem('token') !== null){
+if (localStorage.getItem('token') !== null) {
   let count = 0;
   const socket = io(`${PROTOCOL}://${BACKEND_SERVER_IP}:${BACKEND_SERVER_PORT}/visitors`, {
     auth: {
@@ -6,95 +6,91 @@ if(localStorage.getItem('token') !== null){
     },
   });
 
+  if (window.location.pathname.split("/")[1] !== "posts") {
 
-  const targetElement = document.getElementById("visitors-contents")
-  socket.on('userList', userList => {
-    targetElement.innerHTML = ""
-    count = 0;
-    userList.forEach((user) => {
-      count++;
-      const div = document.createElement("div")
-      div.style.display = "flex"
-      div.style.alignItems = "center"
-      div.style.gap = "10px"
-      div.style.color = "white"
+    const targetElement = document.getElementById("visitors-contents")
+    socket.on('userList', userList => {
+      targetElement.innerHTML = ""
+      count = 0;
+      userList.forEach((user) => {
+        count++;
+        const div = document.createElement("div")
+        div.style.display = "flex"
+        div.style.alignItems = "center"
+        div.style.gap = "10px"
+        div.style.color = "white"
 
-      div.dataset.user = `${user.Users_uid}`
+        div.dataset.user = `${user.Users_uid}`
 
-      const img =  document.createElement("img")
-      img.src = user.Users_profile
-      img.style.width = "50px";
-      img.style.height = "50px"
+        const img = document.createElement("img")
+        img.src = user.Users_profile
+        img.style.width = "50px";
+        img.style.height = "50px"
 
-      const p = document.createElement("p")
-      p.innerText = `${user.Users_nickname}(${user.Role_authority})`
+        const p = document.createElement("p")
+        p.innerText = `${user.Users_nickname}(${user.Role_authority})`
 
-      div.appendChild(img)
-      div.appendChild(p)
-      targetElement.appendChild(div)
-    })
+        div.appendChild(img)
+        div.appendChild(p)
+        targetElement.appendChild(div)
+      })
 
-    const countElement = document.createElement("p")
-    countElement.style.color = "white"
-    countElement.style.textAlign = "center"
-    countElement.style.fontSize = "20px"
-    countElement.style.marginBottom = "20px"
-    countElement.id = "visitors-count"
-
-
-
-
-    countElement.innerText = `${count}명의 유저가 접속 중 입니다.`
-    targetElement.insertBefore(countElement, targetElement.firstChild)
+      const countElement = document.createElement("p")
+      countElement.style.color = "white"
+      countElement.style.textAlign = "center"
+      countElement.style.fontSize = "20px"
+      countElement.style.marginBottom = "20px"
+      countElement.id = "visitors-count"
 
 
+      countElement.innerText = `${count}명의 유저가 접속 중 입니다.`
+      targetElement.insertBefore(countElement, targetElement.firstChild)
 
 
-  });
+    });
 
-  socket.on('userExit', (userList) => {
-    targetElement.innerHTML = ""
-    count = 0;
+    socket.on('userExit', (userList) => {
+      targetElement.innerHTML = ""
+      count = 0;
 
-    userList.forEach((user) => {
-      count++;
-      const div = document.createElement("div")
-      div.style.display = "flex"
-      div.style.alignItems = "center"
-      div.style.gap = "10px"
-      div.style.color = "white"
-
-
-      div.dataset.user = `${user.Users_uid}`
-
-      const img =  document.createElement("img")
-      img.src = user.Users_profile
-      img.style.width = "50px";
-      img.style.height = "50px"
-
-      const p = document.createElement("p")
-      p.innerText = `${user.Users_nickname}(${user.Role_authority})`
-
-      div.appendChild(img)
-      div.appendChild(p)
-      targetElement.appendChild(div)
-    })
-
-    const countElement = document.createElement("p")
-    countElement.style.color = "white"
-    countElement.style.textAlign = "center"
-    countElement.style.fontSize = "20px"
-    countElement.style.marginBottom = "20px"
-    countElement.id = "visitors-count"
+      userList.forEach((user) => {
+        count++;
+        const div = document.createElement("div")
+        div.style.display = "flex"
+        div.style.alignItems = "center"
+        div.style.gap = "10px"
+        div.style.color = "white"
 
 
+        div.dataset.user = `${user.Users_uid}`
+
+        const img = document.createElement("img")
+        img.src = user.Users_profile
+        img.style.width = "50px";
+        img.style.height = "50px"
+
+        const p = document.createElement("p")
+        p.innerText = `${user.Users_nickname}(${user.Role_authority})`
+
+        div.appendChild(img)
+        div.appendChild(p)
+        targetElement.appendChild(div)
+      })
+
+      const countElement = document.createElement("p")
+      countElement.style.color = "white"
+      countElement.style.textAlign = "center"
+      countElement.style.fontSize = "20px"
+      countElement.style.marginBottom = "20px"
+      countElement.id = "visitors-count"
 
 
-    countElement.innerText = `${count}명의 유저가 접속 중 입니다.`
-    targetElement.insertBefore(countElement, targetElement.firstChild)
+      countElement.innerText = `${count}명의 유저가 접속 중 입니다.`
+      targetElement.insertBefore(countElement, targetElement.firstChild)
 
-  });
+    });
 
+  }
 }
 
 
